fba1062bf3d15959edbe48987bceff650a9bf225
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-06 11:47:15 -0600

    Fix testing issue by changing 'rating' value of fixtures to string value

--- test/bookmarks.fixtures.js
+++ test/bookmarks.fixtures.js
@@ -4,22 +4,22 @@ function makeBookmarksArray() {
       id: 1,
       title: "Love Forever",
       url: "www.google.com",
-      descritpion: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-      rating: 9
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      rating: "9"
     },
     {
       id: 2,
       title: "Never Love",
       url: "www.google.com",
-      descritpion: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-      rating: 1
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      rating: "7"
     },
     {
-      id: 1,
+      id: 3,
       title: "Always Love",
       url: "www.google.com",
-      descritpion: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-      rating: 10
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      rating: "10"
     }
   ];
 }
