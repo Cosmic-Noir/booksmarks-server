8158134a060c43709f859f01d1c3c90c17188af1
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-06 10:52:11 -0600

    Add test for non-existing bookmark ID - pass

--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -30,3 +30,14 @@ describe("GET /bookmarks", () => {
     });
   });
 });
+
+describe("GET /bookmarks/:bookmark_id", () => {
+  context("Given no bookmark in db", () => {
+    it("Responds with 404", () => {
+      const bookmarkID = 12455;
+      return supertest(app)
+        .get(`/bookmarks/${bookmarkID}`)
+        .expect(404, { error: { message: `Bookmark doesn't exist` } });
+    });
+  });
+});
