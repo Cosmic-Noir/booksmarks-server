b5119b3dbe51682bcef16abd93e34c3670b8cc6b
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-06 10:25:36 -0600

    Create test for no data in tables - pass

---
+++ test/bookmarks-endpoints.spec.js
@@ -0,0 +1,32 @@
+const { expect } = require("chai");
+const knex = require("knex");
+const app = require("../src/app");
+const { makeBookmarksArray } = require("./bookmarks.fixtures");
+
+let db;
+
+// Create connection
+before("Make knex instance", () => {
+  db = knex({
+    client: "pg",
+    connection: process.env.TEST_DB_URL
+  });
+  app.set("db", db);
+});
+
+// Disconnect and clear the table for testing
+after("Disconnect from db", () => db.destroy());
+
+before("Clear the table", () => db("bookmarks").truncate());
+
+afterEach("Cleanup", () => db("bookmarks").truncate());
+
+describe("GET /bookmarks", () => {
+  context("Given no bookmarks", () => {
+    it("Responds with 200 and an empty list", () => {
+      return supertest(app)
+        .get("/bookmarks")
+        .expect(200, []);
+    });
+  });
+});
